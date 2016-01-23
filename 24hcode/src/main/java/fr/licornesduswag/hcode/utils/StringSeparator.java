6dbd423b75d9e3a27863dd7127bc30b4ee42446b
ckram <cram.leroux@gmail.com>
2016-01-23 19:08:13 +0100

    separator

---
+++ 24hcode/src/main/java/fr/licornesduswag/hcode/utils/StringSeparator.java
@@ -0,0 +1,24 @@
+package fr.licornesduswag.hcode.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class StringSeparator {
+	
+	  public static void main(String[] args) {
+		  StringSeparator.separeString("yolowolo");
+		  
+	  }
+	  
+	static void separeString (String s)
+	{
+		
+		char [] chars = s.toCharArray();
+		ArrayList <Character> charList;
+		for (int i = 0;i<chars.length;i++)
+		{
+			
+		}
+	}
+}
