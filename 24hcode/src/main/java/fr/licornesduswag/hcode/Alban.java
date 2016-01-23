b4fd35ac7c35e9937240fa9edc33bc8e149f4793
Nabnab9 <alban.rousseau9@gmail.com>
2016-01-23 11:04:39 +0100

    ajout classe bidon alban

---
+++ 24hcode/src/main/java/fr/licornesduswag/hcode/Alban.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.licornesduswag.hcode;
+
+/**
+ * @author Alban
+ *
+ */
+public class Alban {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ceci est la classe bidon d'Alban");
+	}
+
+}
