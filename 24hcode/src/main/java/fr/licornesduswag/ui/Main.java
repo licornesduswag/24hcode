bd4e732c88308fca401c48e32936a054a134f118
maximusk <mehdibeniche@gmail.com>
2016-01-23 17:54:49 +0100

    Test piece

--- 24hcode/src/main/java/fr/licornesduswag/ui/Main.java
+++ 24hcode/src/main/java/fr/licornesduswag/ui/Main.java
@@ -25,11 +25,16 @@
 package fr.licornesduswag.ui;
 
 import java.io.InputStream;
+import fr.licornesduswag.ui.Keyboard;
+import fr.licornesduswag.hcode.data.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import java.awt.Font;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -42,14 +47,16 @@ import org.newdawn.slick.util.ResourceLoader;
 
 /**
  *
- * @author Romain Porte (MicroJoe) microjoe at mailoo.org
+ * @author Maximusk
  */
 public class Main extends BasicGame {
     TrueTypeFont font;
     Keyboard k = null;
     boolean test;
     boolean piece;
-    
+    Object elem;
+    String str = "";
+    PieceIterator pi = new PieceIterator(creerPiece());
     public Main() {
         super("24hcode");
     }
@@ -76,6 +83,21 @@ public class Main extends BasicGame {
         if(k.keyDown() && !test && !piece){
            piece = true;
         }
+        else if(k.keyDown() && !test){
+           elem = pi.next();
+           switch(elem.getClass().getName()){
+                case("fr.licornesduswag.hcode.data.Acte"):
+                   Acte a = (Acte)elem;
+                   str = "Acte " + a.getNumero();
+                   break;
+                case("fr.licornesduswag.hcode.data.Scene"):
+                   Scene s = (Scene)elem;
+                   str = "Scène " + s.getNumero();
+                   break;
+                default:
+                   str = "Autre";               
+           }
+        }
         test = !k.keyUp();
     }
 
@@ -92,7 +114,7 @@ public class Main extends BasicGame {
             grphcs.setBackground(Color.cyan);
             grphcs.setColor(Color.darkGray);
             grphcs.setFont(font);
-            grphcs.drawString("ceci est une pièce", 10, 10);
+            grphcs.drawString(str, 10, 10);
         }
     }
     
@@ -106,4 +128,92 @@ public class Main extends BasicGame {
             e.printStackTrace();
         }
     }
+    
+    public Piece creerPiece(){
+        System.out.println("Salut !");
+         
+        // Texte
+        Texte txt = new Texte("Roméo, oh Roméo !");
+        
+        //Personnage
+        Personnage perso1 = new Personnage("Roméo", "toto.png", "tata.png");
+        Personnage perso2 = new Personnage("Juliette", "toto.png", "tata.png");
+        
+        // Réplique
+        ArrayList<Contenu> contenus = new ArrayList<>();
+        contenus.add(txt);
+        Replique rep = new Replique(contenus);
+        
+        // Dialogue
+        ArrayList<Replique> repliques = new ArrayList<>();
+        repliques.add(rep);
+        Dialogue dial = new Dialogue(repliques);
+        
+        // Scene
+        ArrayList<Dialogue> dialogues = new ArrayList<>();
+        dialogues.add(dial);
+        Scene scene = new Scene(1, dialogues);
+        
+        // Acte
+        ArrayList<Scene> scenes = new ArrayList<>();
+        scenes.add(scene);
+        Acte acte = new Acte(1, scenes);
+        
+        // Pièce
+        ArrayList<Acte> actes = new ArrayList<>();
+        ArrayList<Personnage> personnages = new ArrayList<>();
+        actes.add(acte);
+        personnages.add(perso1);
+        personnages.add(perso2);
+        Piece piece = new Piece("Romeo et Juliette", actes, personnages);
+        Personnage sgana = new Personnage("sgana", "SganarelleFace"	, "SganarelleAventure");
+        Personnage martine = new Personnage("martine","martineFace","martineAventure");
+        Personnage mRobert = new Personnage("M.robert", "teteDeMRobert", "MRobertAventure");
+        personnages.clear();
+        personnages.add(sgana);
+        personnages.add(martine);
+        repliques = new ArrayList<>();
+        txt = new Texte("Non, je te dis que je n’en veux rien faire, et que c’est à moi de parler et d’être le maître.");
+        contenus = new ArrayList<>();
+        contenus.add(txt);
+        
+        repliques.add(new Replique(contenus,sgana.getNom()));
+        contenus = new ArrayList<>();
+        contenus.add(new Texte("Et je te dis, moi, que je veux que tu vives à ma fantaisie, et ne je ne me suis point mariée avec toi pour souffrir tes fredaines. "));
+        repliques.add(new Replique(contenus,martine.getNom()));
+        
+        dial = new Dialogue(repliques,personnages);
+        scene.getDialogues().add(dial);
+        scenes.add(scene);
+        scene = new Scene(2, new ArrayList<Dialogue>());
+        personnages = new ArrayList<Personnage>();
+        personnages.add(mRobert);
+        personnages.add(sgana);
+        personnages.add(martine);
+        repliques = new ArrayList<>();
+        contenus = new ArrayList<>();
+        contenus.add(new Texte("Holà, holà, holà ! Fi ! Qu’est-ce ci ? Quelle infamie ! Peste soit le coquin, de battre ainsi sa femme ! "));
+        
+        repliques.add(new Replique());
+        dial= new Dialogue(repliques, personnages);
+        System.out.println(piece);
+        
+        
+        
+        
+        
+        
+        //Serializer
+        Serializer serial = new Serializer(piece);
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        try {
+			serial.toZip("test.zip","../sprites/Medecin malgre lui/Persos/");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        System.out.println(piece);
+        return(piece);
+    }    
 }
