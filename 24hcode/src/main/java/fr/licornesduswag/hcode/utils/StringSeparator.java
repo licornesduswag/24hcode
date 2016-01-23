bb622e5dbacfe962d878d4a7601a5b1458257640
Romain Porte <microjoe@mailoo.org>
2016-01-23 22:07:30 +0100

    Texte pas coupé

--- 24hcode/src/main/java/fr/licornesduswag/hcode/utils/StringSeparator.java
+++ 24hcode/src/main/java/fr/licornesduswag/hcode/utils/StringSeparator.java
@@ -3,7 +3,7 @@ package fr.licornesduswag.hcode.utils;
 public class StringSeparator {
 	
 	  public static void main(String[] args) {
-		  StringSeparator.separeString("yolowolofdks,fklq,klq,slkf,klqs,fk,qld,fklq,fkl,qkl,f,qls,fkl,qkfl,kl,flk,klq,kgnknqklnqkfngj,q,gsklf,lknsqkgn,qeùqg,",5);
+		  StringSeparator.separeString("yolowolofdks,fklq,klq,slkf,klqs,fk,qld,fklq,fkl,qkl,f,qls,fkl,qkfl,kl,flk,klq,kgnknqklnqkfngj,q,gsklf,lknsqkgn,qeùqg,".replace(',', ' '),20);
 		  
 	  }
 	  /**
@@ -12,20 +12,35 @@ public class StringSeparator {
 	   * @param taille tailleMax dans laquelle la string est séparée
 	   * @return la string avec les \n
 	   */
-	static String separeString (String s, int taille)
+	public static String separeString (String s, int taille)
 	{
-		
 		char [] chars = s.toCharArray();
-		String toSend= new String();
-		for (int i = 0;i<chars.length;i++)
-		{			
-			toSend+=chars[i];
-			if ((i+1)%taille==0 && i!=1)
-			{				
-				
-				toSend+='\n';				
-			}			
-		}				
+        
+		String toSend = new String();
+		int start = 0;
+        int stop = 0;
+        int prevStop = 0;
+        
+        while (stop < chars.length) {
+            // Tant qu'on a pas un espace on avance
+            while (chars[stop] != ' ') {
+                stop++;
+            }
+            
+            // Si la taille est suffisemment grande
+            if (stop - start > taille) {
+                String line = new String(chars, start, prevStop - start);
+                System.out.println(line);
+                toSend += line + '\n';
+                start = prevStop;
+            } else {
+                // Sinon on avance
+                stop++;
+            }
+            
+            prevStop = stop;
+        }
+
 		System.out.println(toSend);
 		return toSend;
 	}
