2970742fb7b6b7c1b0fe4204fdd9a744d4c7bcbb
Romain Porte <microjoe@mailoo.org>
2016-01-23 12:00:28 +0100

    Deplacement dans le bon package

--- 24hcode/src/main/java/fr/licornesduswag/hcode/RTFReader/Main.java
+++ 24hcode/src/main/java/fr/licornesduswag/hcode/RTFReader/Main.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package fr.licornesduswag.RTFReader;
+package fr.licornesduswag.hcode.RTFReader;
 
 import java.io.File;
 import java.io.FileNotFoundException;
